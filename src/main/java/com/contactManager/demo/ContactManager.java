38d69da5e25eced917b726b3453fbe38d40d0bf1
Ruturaj-007 <pruturaj3003@gmail.com>
2026-02-01 12:50:41 +0530

    feat: added ConcurrentHashMap & methods to prevent duplicate entries in ContactManager

---
+++ src/main/java/com/contactManager/demo/ContactManager.java
@@ -0,0 +1,38 @@
+package com.contactManager.demo;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ContactManager {
+    // ConcurrentHashMap is thread-safe & allows concurrent reads, writes & prevents race condition
+    private Map<String, Contact> contactList = new ConcurrentHashMap<>();
+
+    public void addContact(String firstName, String lastName, String phoneNumber) {
+    Contact contact = new Contact(firstName, lastName, phoneNumber);
+    validateContact(contact);
+    checkIfContactAlreadyExists(contact);
+    contactList.put(generateKey(contact), contact);
+    }
+
+    public Collection<Contact> getAllContacts() {
+        return contactList.values();
+    }
+
+    private void validateContact(Contact contact) {
+        contact.validateFirstName();
+        contact.validateLastName();
+        contact.validatePhoneNumber();
+    }
+
+    private void checkIfContactAlreadyExists(Contact contact) {
+        // Duplicate entries are prevented
+        if (contactList.containsKey(generateKey(contact))) {
+            throw new RuntimeException("Contact already exists");
+        }
+    }
+
+    private String generateKey(Contact contact) {
+        return String.format("%s-%s", contact.getFirstName(), contact.getLastName());
+    }
+}
